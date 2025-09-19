37f6caf1e23fe4594b56a88a1a9e7e7a91c1e3ad
Twisper <83642379+Twisper@users.noreply.github.com>
2025-09-19 20:49:03 +0300

    Feat(ch05): Added script for Linux API Headers

---
+++ scripts/ch05/03-linux-api-headers.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+LOG_DIR="/home/twisper/lfs_project/logs/ch05/03-linux-api-headers"
+mkdir -p "$LOG_DIR"
+echo "Created logging directory"
+
+cd $LFS/sources
+
+tar -xf linux-6.16.1.tar.xz
+cd linux-6.16.1
+
+echo "Cleaning sources"
+make mrproper 2>&1 | tee "$LOG_DIR/mrproper.log"
+echo "Done cleaning sources"
+
+echo "Installing headers"
+make headers 2>&1 | tee "$LOG_DIR/headers.log"
+echo "Done installing headers"
+
+echo "Cleaning all directories"
+cd $LFS/sources
+rm -rfv linux-6.16.1
+echo "All done"
