4eb0f38618ed008efe2db54dbc52f695c3bfc8f1
Twisper <83642379+Twisper@users.noreply.github.com>
2025-09-20 23:21:12 +0300

    Feat(ch06): Added script for gcc build pass2

---
+++ scripts/ch06/17-gcc-pass2.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+set -e
+
+PKT_NUM="17"
+PKT_NAME="gcc"
+VER_NAME="15.2.0"
+ARH_NAME="tar.xz"
+LOG_DIR="/home/twisper/lfs_project/logs/ch06/${PKT_NUM}-${PKT_NAME}"
+FLD_NAME="${PKT_NAME}-${VER_NAME}"
+TAR_NAME="$FLD_NAME.${ARH_NAME}"
+
+mkdir -p "$LOG_DIR"
+echo "Created logging directory"
+
+cd $LFS/sources
+
+echo "Unpacking ${PKT_NAME}"
+tar -xf "$TAR_NAME"
+cd "$FLD_NAME"
+echo "Unpacking done"
+
+tar -xf ../mpfr-4.2.2.tar.xz
+mv -v mpfr-4.2.2 mpfr
+tar -xf ../gmp-6.3.0.tar.xz
+mv -v gmp-6.3.0 gmp
+tar -xf ../mpc-1.3.1.tar.gz
+mv -v mpc-1.3.1 mpc
+
+case $(uname -m) in
+  x86_64)
+    sed -e '/m64=/s/lib64/lib/' \
+        -i.orig gcc/config/i386/t-linux64
+  ;;
+esac
+
+sed '/thread_header =/s/@.*@/gthr-posix.h/' \
+    -i libgcc/Makefile.in libstdc++-v3/include/Makefile.in
+
+mkdir -v build
+cd       build
+
+(
+    ../configure                   \
+    --build=$(../config.guess) \
+    --host=$LFS_TGT            \
+    --target=$LFS_TGT          \
+    --prefix=/usr              \
+    --with-build-sysroot=$LFS  \
+    --enable-default-pie       \
+    --enable-default-ssp       \
+    --disable-nls              \
+    --disable-multilib         \
+    --disable-libatomic        \
+    --disable-libgomp          \
+    --disable-libquadmath      \
+    --disable-libsanitizer     \
+    --disable-libssp           \
+    --disable-libvtv           \
+    --enable-languages=c,c++   \
+    LDFLAGS_FOR_TARGET=-L$PWD/$LFS_TGT/libgcc
+) 2>&1 | tee "$LOG_DIR/configure.log"
+
+echo "Building and installing ${PKT_NAME}"
+make 2>&1 | tee "$LOG_DIR/make.log"
+make DESTDIR=$LFS install | tee 2>&1 "$LOG_DIR/install.log"
+
+ln -sv gcc $LFS/usr/bin/cc
+
+echo "Installing done. Cleaning up"
+cd "$LFS/sources"
+rm -rfv "$FLD_NAME"
+echo "All done"
