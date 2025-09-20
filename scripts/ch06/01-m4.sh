4ef2a46ec41e4381521f6d8f7108b7b45d1f7a2a
Twisper <83642379+Twisper@users.noreply.github.com>
2025-09-20 14:27:03 +0300

    Feat(ch06): Added script for M4 build

---
+++ scripts/ch06/01-m4.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+
+LOG_DIR="/home/twisper/lfs_project/logs/ch06/01-m4"
+mkdir -p "$LOG_DIR"
+echo "Created logging directory"
+
+cd $LFS/sources
+
+echo "Unpacking M4"
+tar -xf "M4-1.4.20.tar.xz"
+cd M4-1.4.20
+echo "Unpacking is done"
+
+(./configure --prefix=/usr   \
+            --host=$LFS_TGT \
+            --build=$(build-aux/config.guess)) 2>&1 | tee "$LOG_DIR/configure.log"
+
+echo "Compiling and installing M4"
+make 2>&1 | tee "$LOG_DIR/make.log"
+make DESTDIR=$LFS install 2>&1 | tee "$LOG_DIR/install.log"
+echo "Installing complete"
+
+echo "Cleaning up..."
+cd $LFS/sources
+rm -rfv M4-1.4.20
+echo "All done"
