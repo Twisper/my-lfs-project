1de5af8ea79986218f0aef6996cbc8887e4690f1
Twisper <83642379+Twisper@users.noreply.github.com>
2025-09-21 15:37:18 +0300

    Feat(ch07) Added script for Python build and install

---
+++ scripts/ch07/04-python.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -e
+
+CHPTR_NUM="07"
+PKT_NUM="04"
+PKT_NAME="Python"
+VER_NAME="3.13.7"
+ARH_NAME="tar.xz"
+LOG_DIR="/lfs_project/logs/ch${CHPTR_NUM}/${PKT_NUM}-${PKT_NAME}"
+FLD_NAME="${PKT_NAME}-${VER_NAME}"
+TAR_NAME="$FLD_NAME.${ARH_NAME}"
+
+mkdir -p "$LOG_DIR"
+echo "Created logging directory"
+
+cd /sources
+
+echo "Unpacking ${PKT_NAME}"
+tar -xf "$TAR_NAME"
+cd "$FLD_NAME"
+echo "Unpacking done"
+
+(
+    ./configure --prefix=/usr       \
+            --enable-shared     \
+            --without-ensurepip \
+            --without-static-libpython
+) 2>&1 | tee "$LOG_DIR/configure.log"
+
+echo "Building and installing ${PKT_NAME}"
+make 2>&1 | tee "$LOG_DIR/make.log"
+make install 2>&1 | tee "$LOG_DIR/install.log"
+
+echo "Installing done. Cleaning up"
+cd "/sources"
+rm -rfv "$FLD_NAME"
+echo "All done"
