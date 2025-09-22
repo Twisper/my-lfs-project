c9b8a7607d8d4c60be96ec6a07f7f0ac50f36c6b
Twisper <83642379+Twisper@users.noreply.github.com>
2025-09-22 22:29:09 +0300

    Feat(ch08): Added script for ncurses building and installing

---
+++ scripts/ch08/28-ncurses.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+set -e
+
+PKT_NAME=$(basename "$0" | sed -e 's/^[0-9]\+-//' -e 's/\.sh$//')
+
+PKT_NUM=$(basename "$0" | sed 's/-.*//')
+
+CHPTR_DIR=$(dirname "$0")
+CHPTR_NUM=$(basename "$CHPTR_DIR" | sed 's/ch//')
+
+TAR_NAME=$(find . -maxdepth 1 -name "${PKT_NAME}-*.tar.*" | head -n 1)
+
+if [[ -z "$TAR_NAME" ]]; then
+    echo "ERROR: No package with name ${PKT_NAME} found in /sources!"
+    exit 1
+fi
+
+FLD_NAME=$(basename "$TAR_NAME" | sed 's/\.tar\..*$//')
+VER_NAME=$(echo "$FLD_NAME" | sed "s/^${PKT_NAME}-//")
+
+LOG_DIR="/lfs_project/logs/ch${CHPTR_NUM}/${PKT_NUM}-${PKT_NAME}"
+
+
+
+mkdir -p "$LOG_DIR"
+echo "Created logging directory"
+
+cd /sources
+
+echo "Unpacking ${PKT_NAME}"
+tar -xf "$TAR_NAME"
+cd "$FLD_NAME"
+echo "Unpacking done"
+
+(
+    ./configure --prefix=/usr           \
+            --mandir=/usr/share/man \
+            --with-shared           \
+            --without-debug         \
+            --without-normal        \
+            --with-cxx-shared       \
+            --enable-pc-files       \
+            --with-pkg-config-libdir=/usr/lib/pkgconfig
+) 2>&1 | tee "$LOG_DIR/configure.log"
+
+echo "Compiling ${PKT_NAME}"
+make 2>&1 | tee "$LOG_DIR/make.log"
+
+echo "Installing ${PKT_NAME}"
+make DESTDIR=$PWD/dest install 2>&1 | tee "$LOG_DIR/install.log"
+install -vm755 dest/usr/lib/libncursesw.so.6.5 /usr/lib
+rm -v  dest/usr/lib/libncursesw.so.6.5
+sed -e 's/^#if.*XOPEN.*$/#if 1/' \
+    -i dest/usr/include/curses.h
+cp -av dest/* /
+
+for lib in ncurses form panel menu ; do
+    ln -sfv lib${lib}w.so /usr/lib/lib${lib}.so
+    ln -sfv ${lib}w.pc    /usr/lib/pkgconfig/${lib}.pc
+done
+
+ln -sfv libncursesw.so /usr/lib/libcurses.so
+cp -v -R doc -T /usr/share/doc/ncurses-6.5-20250809
+
+echo "Installing done. Cleaning up"
+cd "/sources"
+rm -rfv "$FLD_NAME"
+echo "All done"
