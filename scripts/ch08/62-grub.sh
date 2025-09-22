8bb0614d3189434f0a7517baccc20d146d5969f9
Twisper <83642379+Twisper@users.noreply.github.com>
2025-09-23 00:08:53 +0300

    Feat(ch08): Added script for grub building and installing

---
+++ scripts/ch08/62-grub.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+set -e
+
+PKT_NAME=$(basename "$0" | sed -e 's/^[0-9]\+-//' -e 's/\.sh$//')
+
+PKT_NUM=$(basename "$0" | sed 's/-.*//')
+
+CHPTR_DIR=$(dirname "$0")
+CHPTR_NUM=$(basename "$CHPTR_DIR" | sed 's/ch//')
+
+TAR_NAME=$(find . -maxdepth 1 -name "${PKT_NAME}-*.tar.*" | head -n 1)
+
+if [[ -z "$TAR_NAME" ]]; then
+    echo "ERROR: No package with name ${PKT_NAME} found in /sources!"
+    exit 1
+fi
+
+FLD_NAME=$(basename "$TAR_NAME" | sed 's/\.tar\..*$//')
+VER_NAME=$(echo "$FLD_NAME" | sed "s/^${PKT_NAME}-//")
+
+LOG_DIR="/lfs_project/logs/ch${CHPTR_NUM}/${PKT_NUM}-${PKT_NAME}"
+
+
+
+unset {C,CPP,CXX,LD}FLAGS
+
+mkdir -p "$LOG_DIR"
+echo "Created logging directory"
+
+cd /sources
+
+echo "Unpacking ${PKT_NAME}"
+tar -xf "$TAR_NAME"
+cd "$FLD_NAME"
+echo "Unpacking done"
+
+echo depends bli part_gpt > grub-core/extra_deps.lst
+
+(
+    ./configure --prefix=/usr     \
+            --sysconfdir=/etc \
+            --disable-efiemu  \
+            --disable-werror
+) 2>&1 | tee "$LOG_DIR/configure.log"
+
+echo "Compiling and testing ${PKT_NAME}"
+make 2>&1 | tee "$LOG_DIR/make.log"
+
+echo "Installing ${PKT_NAME}"
+make install 2>&1 | tee "$LOG_DIR/install.log"
+mv -v /etc/bash_completion.d/grub /usr/share/bash-completion/completions
+
+echo "Installing done. Cleaning up"
+cd "/sources"
+rm -rfv "$FLD_NAME"
+echo "All done"
