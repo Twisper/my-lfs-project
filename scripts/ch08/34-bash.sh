53314123485e8ecc18e38625ffe4539210c835c9
Twisper <83642379+Twisper@users.noreply.github.com>
2025-09-22 22:29:11 +0300

    Feat(ch08): Added script for bash building and installing

---
+++ scripts/ch08/34-bash.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+set -e
+
+PKT_NAME=$(basename "$0" | sed -e 's/^[0-9]\+-//' -e 's/\.sh$//')
+
+PKT_NUM=$(basename "$0" | sed 's/-.*//')
+
+CHPTR_DIR=$(dirname "$0")
+CHPTR_NUM=$(basename "$CHPTR_DIR" | sed 's/ch//')
+
+TAR_NAME=$(find . -maxdepth 1 -name "${PKT_NAME}-*.tar.*" | head -n 1)
+
+if [[ -z "$TAR_NAME" ]]; then
+    echo "ERROR: No package with name ${PKT_NAME} found in /sources!"
+    exit 1
+fi
+
+FLD_NAME=$(basename "$TAR_NAME" | sed 's/\.tar\..*$//')
+VER_NAME=$(echo "$FLD_NAME" | sed "s/^${PKT_NAME}-//")
+
+LOG_DIR="/lfs_project/logs/ch${CHPTR_NUM}/${PKT_NUM}-${PKT_NAME}"
+
+
+
+mkdir -p "$LOG_DIR"
+echo "Created logging directory"
+
+cd /sources
+
+echo "Unpacking ${PKT_NAME}"
+tar -xf "$TAR_NAME"
+cd "$FLD_NAME"
+echo "Unpacking done"
+
+(
+    ./configure --prefix=/usr             \
+            --without-bash-malloc     \
+            --with-installed-readline \
+            --docdir=/usr/share/doc/bash-5.3
+) 2>&1 | tee "$LOG_DIR/configure.log"
+
+echo "Compiling and testing ${PKT_NAME}"
+make 2>&1 | tee "$LOG_DIR/make.log"
+
+echo "Compiling is done, check for results. Enter this:"
+echo "LC_ALL=C.UTF-8 su -s /usr/bin/expect tester << "EOF""
+echo "set timeout -1"
+echo "spawn make tests"
+echo "expect eof"
+echo "lassign [wait] _ _ _ value"
+echo "exit $value"
+echo "EOF"
+read -p "> "
+
+echo "Installing ${PKT_NAME}"
+make install 2>&1 | tee "$LOG_DIR/install.log"
+
+echo "Installing done. Cleaning up"
+cd "/sources"
+rm -rfv "$FLD_NAME"
+echo "All done. Enter exec /usr/bin/bash --login"
