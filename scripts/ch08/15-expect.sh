69f9bf931631bd1b81697af0f5605fa8fb2f3cfd
Twisper <83642379+Twisper@users.noreply.github.com>
2025-09-22 22:29:07 +0300

    Feat(ch08): Added script for expect building and installing

---
+++ scripts/ch08/15-expect.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+set -e
+
+PKT_NAME=$(basename "$0" | sed -e 's/^[0-9]\+-//' -e 's/\.sh$//')
+
+PKT_NUM=$(basename "$0" | sed 's/-.*//')
+
+CHPTR_DIR=$(dirname "$0")
+CHPTR_NUM=$(basename "$CHPTR_DIR" | sed 's/ch//')
+
+TAR_NAME=$(find . -maxdepth 1 -name "${PKT_NAME}-*.tar.*" | head -n 1)
+
+if [[ -z "$TAR_NAME" ]]; then
+    echo "ERROR: No package with name ${PKT_NAME} found in /sources!"
+    exit 1
+fi
+
+FLD_NAME=$(basename "$TAR_NAME" | sed 's/\.tar\..*$//')
+VER_NAME=$(echo "$FLD_NAME" | sed "s/^${PKT_NAME}-//")
+
+LOG_DIR="/lfs_project/logs/ch${CHPTR_NUM}/${PKT_NUM}-${PKT_NAME}"
+
+
+
+mkdir -p "$LOG_DIR"
+echo "Created logging directory"
+
+cd /sources
+
+echo "Unpacking ${PKT_NAME}"
+tar -xf "$TAR_NAME"
+cd "$FLD_NAME"
+echo "Unpacking done"
+
+python3 -c 'from pty import spawn; spawn(["echo", "ok"])'
+
+patch -Np1 -i ../expect-5.45.4-gcc15-1.patch
+
+(
+    ./configure --prefix=/usr           \
+            --with-tcl=/usr/lib     \
+            --enable-shared         \
+            --disable-rpath         \
+            --mandir=/usr/share/man \
+            --with-tclinclude=/usr/include
+) 2>&1 | tee "$LOG_DIR/configure.log"
+
+echo "Compiling and testing ${PKT_NAME}"
+make 2>&1 | tee "$LOG_DIR/make.log"
+make test 2>&1 | tee "$LOG_DIR/check.log"
+
+echo "Testing is done, check for results"
+read -p "> "
+echo "Installing ${PKT_NAME}"
+make install 2>&1 | tee "$LOG_DIR/install.log"
+
+ln -svf expect5.45.4/libexpect5.45.4.so /usr/lib
+
+echo "Installing done. Cleaning up"
+cd "/sources"
+rm -rfv "$FLD_NAME"
+echo "All done"
