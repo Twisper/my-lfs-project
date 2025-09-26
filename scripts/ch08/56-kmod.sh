b08d3ebd4a4bd5df67ac892112914e27402093d7
Twisper <83642379+Twisper@users.noreply.github.com>
2025-09-27 01:17:32 +0300

    Fix(ch08): Added script for kmod build

---
+++ scripts/ch08/56-kmod.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+set -e
+
+PKT_NAME=$(basename "$0" | sed -e 's/^[0-9]\+-//' -e 's/\.sh$//')
+
+PKT_NUM=$(basename "$0" | sed 's/-.*//')
+
+CHPTR_DIR=$(dirname "$0")
+CHPTR_NUM=$(basename "$CHPTR_DIR" | sed 's/ch//')
+
+TAR_NAME=$(find . -maxdepth 1 -name "${PKT_NAME}-*.tar.*" | head -n 1)
+
+if [[ -z "$TAR_NAME" ]]; then
+    echo "ERROR: No package with name ${PKT_NAME} found in /sources!"
+    exit 1
+fi
+
+FLD_NAME=$(basename "$TAR_NAME" | sed 's/\.tar\..*$//')
+VER_NAME=$(echo "$FLD_NAME" | sed "s/^${PKT_NAME}-//")
+
+LOG_DIR="/lfs_project/logs/ch${CHPTR_NUM}/${PKT_NUM}-${PKT_NAME}"
+
+
+
+mkdir -p "$LOG_DIR"
+echo "Created logging directory"
+
+cd /sources
+
+echo "Unpacking ${PKT_NAME}"
+tar -xf "$TAR_NAME"
+cd "$FLD_NAME"
+echo "Unpacking done"
+
+mkdir -p build
+cd       build
+
+(
+    meson setup --prefix=/usr ..    \
+            --buildtype=release \
+            -D manpages=false
+) 2>&1 | tee "$LOG_DIR/configure.log"
+
+echo "Compiling and testing ${PKT_NAME}"
+ninja 2>&1 | tee "$LOG_DIR/make.log"
+
+echo "Installing ${PKT_NAME}"
+ninja install 2>&1 | tee "$LOG_DIR/install.log"
+
+echo "Installing done. Cleaning up"
+cd "/sources"
+rm -rfv "$FLD_NAME"
+echo "All done"
