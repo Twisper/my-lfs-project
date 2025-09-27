e73768bff587521a8fea9b06e1af6a24dff0bdc3
Twisper <83642379+Twisper@users.noreply.github.com>
2025-09-27 17:04:27 +0300

    Feat(ch10): Added line for EFI partition

--- scripts/ch10/fstab.sh
+++ scripts/ch10/fstab.sh
@@ -1,11 +1,18 @@
-cat > /etc/fstab << "EOF"
+#!/bin/bash
+
+LFS_ROOT_UUID="723c654c-83ee-4c4f-94e8-3f41558456ff"
+LFS_SWAP_UUID="b506ff07-8e4a-4692-b864-a92432084d49"
+LFS_ESP_UUID="2580-C938"
+
+cat > /etc/fstab << EOF
 # Begin /etc/fstab
 
 # file system  mount-point    type     options             dump  fsck
 #                                                                order
 
-/dev/sda1     /              ext4     defaults            1     1
-/dev/sdb3     swap           swap     pri=1               0     0
+UUID=${LFS_ROOT_UUID}     /              ext4     defaults            1     1
+UUID=${LFS_SWAP_UUID}     swap           swap     pri=1               0     0
+UUID=${LFS_ESP_UUID} /boot/efi vfat codepage=437,iocharset=iso8859-1 0 1
 proc           /proc          proc     nosuid,noexec,nodev 0     0
 sysfs          /sys           sysfs    nosuid,noexec,nodev 0     0
 devpts         /dev/pts       devpts   gid=5,mode=620      0     0
