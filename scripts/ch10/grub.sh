d3e8d8c63ee23979f5af00e9d5cf85657931eb2c
Twisper <83642379+Twisper@users.noreply.github.com>
2025-09-27 17:11:34 +0300

    Feat(ch10): Added script for GRUB configuring

---
+++ scripts/ch10/grub.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+LFS_ROOT_UUID="723c654c-83ee-4c4f-94e8-3f41558456ff"
+
+cat > /boot/grub/grub.cfg << EOF
+# Begin /boot/grub/grub.cfg
+set default=0
+set timeout=5
+
+insmod part_gpt
+insmod ext2
+set root=(hd0,2)
+
+insmod efi_gop
+insmod efi_uga
+if loadfont /boot/grub/fonts/unicode.pf2; then
+  terminal_output gfxterm
+fi
+
+menuentry "GNU/Linux, Linux 6.16.1-lfs-12.4" {
+  linux   /boot/vmlinuz-6.16.1-lfs-12.4 root=UUID=${LFS_ROOT_UUID} ro
+}
+
+menuentry "Firmware Setup" {
+  fwsetup
+}
+EOF
